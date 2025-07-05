a3b875db13f90622109e1b25101c7999dbbd1cfe
abhro <5664668+abhro@users.noreply.github.com>
2025-07-05 09:43:47 -0400

    Fix imports and module structure

--- src/debug.jl
+++ src/debug.jl
@@ -1,7 +1,11 @@
-#export energy_density, therm_energy_density, zone_vol, energy_pool
+module debug
+export energy_density, therm_energy_density, zone_vol, energy_pool
 
-energy_density = zeros(n_grid, n_ions)
-therm_energy_density = zeros(n_grid, n_ions)
+import ..n_grid, ..n_ions
 
-zone_vol = zeros(n_grid)
-energy_pool = zeros(n_grid)
+const energy_density = zeros(n_grid, n_ions)
+const therm_energy_density = zeros(n_grid, n_ions)
+
+const zone_vol = zeros(n_grid)
+const energy_pool = zeros(n_grid)
+end
