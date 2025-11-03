826a4c870d8f502e3aa7053a7ed96b14d41834a8
Abhro <5664668+abhro@users.noreply.github.com>
2025-11-03 12:57:21 -0500

    Rename constants

--- src/photon_calcs.jl
+++ src/photon_calcs.jl
@@ -7,16 +7,16 @@ using .parameters: psd_max, na_photons
 
 # To make the resultant spectra easier to add, set min and max energies
 # for the overall spectrum and the individual emission spectra.
-const E_photon_min = 1e-13MeV
-const E_photon_max = 1e+12MeV
-const bins_per_dec_photon   = 10
+const Eᵧ_min = 1e-13MeV
+const Eᵧ_max = 1e+12MeV
+const bins_per_dec_photon = 10
 
-const E_photon_pion_min  = 1.0MeV
-const E_photon_pion_max  = E_photon_max
-const E_photon_synch_min = E_photon_min
-const E_photon_synch_max = 1e5MeV
-const E_photon_ic_min    = 1e-2MeV
-const E_photon_ic_max    = E_photon_max
+const Eᵧ_pion_min  = 1MeV
+const Eᵧ_pion_max  = Eᵧ_max
+const Eᵧ_synch_min = Eᵧ_min
+const Eᵧ_synch_max = 1e5MeV
+const Eᵧ_IC_min    = 1e-2MeV
+const Eᵧ_IC_max    = Eᵧ_max
 
 """
     photon_calcs(...)
@@ -40,21 +40,21 @@ function photon_calcs(
     dist_lum = ustrip(cm, jet_dist_kpc*(1 + redshift) * kpc)
 
     # Check to make sure that the arrays can store the emission spectra
-    if log10(E_photon_max / E_photon_min) * bins_per_dec_photon > na_photons
+    if log10(Eᵧ_max / Eᵧ_min) * bins_per_dec_photon > na_photons
         error("Photon arrays will not be big enough. Check na_photons, compare against photon_energy_***_MeV.")
     end
 
     # Set the number of bins used in the individual emission spectra
-    n_photon_pion  = trunc(Int, log10(E_photon_pion_max  / E_photon_pion_min)  * bins_per_dec_photon)
-    n_photon_synch = trunc(Int, log10(E_photon_synch_max / E_photon_synch_min) * bins_per_dec_photon)
-    n_photon_IC    = trunc(Int, log10(E_photon_ic_max    / E_photon_ic_min)    * bins_per_dec_photon)
+    n_photon_pion  = trunc(Int, log10(Eᵧ_pion_max  / Eᵧ_pion_min)  * bins_per_dec_photon)
+    n_photon_synch = trunc(Int, log10(Eᵧ_synch_max / Eᵧ_synch_min) * bins_per_dec_photon)
+    n_photon_IC    = trunc(Int, log10(Eᵧ_IC_max    / Eᵧ_IC_min)    * bins_per_dec_photon)
 
     n_shells = num_upstream_shells + num_downstream_shells
 
     num_hist_bins = psd_max
-    p_pf_therm = zeros(0:num_hist_bins)
+    p_pf_therm     = zeros(0:num_hist_bins)
     dNdp_pf_therm  = zeros(0:num_hist_bins)
-    p_pf_cr    = zeros(0:num_psd_mom_bins+1)
+    p_pf_cr        = zeros(0:num_psd_mom_bins+1)
     dNdp_pf_cr     = zeros(0:num_psd_mom_bins+1)
     cos_bounds     = zeros(0:num_psd_θ_bins+1)
     d²Ndp_slice    = zeros(0:psd_max, 0:psd_max)
@@ -80,7 +80,7 @@ function photon_calcs(
             if (count(>(1e-99), dNdp_pf_therm) + count(>(1e-99), dNdp_pf_cr)) ≥ 1
                 photon_pion_decay(n, num_hist_bins, p_pf_therm,
                                   dNdp_pf_therm, num_psd_mom_bins, p_pf_cr, dNdp_pf_cr,
-                                  n_photon_pion, E_photon_pion_min, bins_per_dec_photon, dist_lum, redshift)
+                                  n_photon_pion, Eᵧ_pion_min, bins_per_dec_photon, dist_lum, redshift)
             end
 
         else # Synchrotron and IC emission from electrons
@@ -103,7 +103,7 @@ function photon_calcs(
                 photon_synch(
                     n, num_hist_bins, p_pf_therm, dNdp_pf_therm,
                     num_psd_mom_bins, p_pf_cr, dNdp_pf_cr, n_photon_synch,
-                    E_photon_synch_min, bins_per_dec_photon, dist_lum, redshift,
+                    Eᵧ_synch_min, bins_per_dec_photon, dist_lum, redshift,
                     n_ions, aa_ion, n₀_ion, γ₀, u₀, flux_px_upstream, flux_energy_upstream, u₂, btot_grid,
                     i_ion, mc,
                 )
@@ -128,7 +128,7 @@ function photon_calcs(
             # particle was present in this grid zone
             if count(d²Ndp_slice .> 1e-99) ≥ 1
                 photon_IC(n, p_pf_cr, num_psd_mom_bins, cos_bounds, num_psd_θ_bins, d²Ndp_slice,
-                          n_photon_IC, E_photon_ic_min, bins_per_dec_photon, dist_lum, redshift)
+                          n_photon_IC, Eᵧ_IC_min, bins_per_dec_photon, dist_lum, redshift)
             end
 
         end  # check on aa
@@ -145,8 +145,8 @@ function photon_calcs(
         # calculated in ISM frame, everything else in plasma). They must be processed and
         # combined into emission viewed in a single reference frame.
         get_summed_emission(n_shells, n_shell_endpoints, n_photon_pion, n_photon_synch, n_photon_IC,
-                            E_photon_min, E_photon_max,
-                            bins_per_dec_photon, E_photon_pion_min,
-                            E_photon_synch_min, E_photon_ic_min)
+                            Eᵧ_min, Eᵧ_max,
+                            bins_per_dec_photon, Eᵧ_pion_min,
+                            Eᵧ_synch_min, Eᵧ_IC_min)
     end
 end
